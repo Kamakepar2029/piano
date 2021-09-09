24cd696ae36cde0bc70609230801045f3c450428
Programmer <kamakepar@kamakepar.ru>
2021-09-09 15:37:31 +0300

    ASS

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,55 @@ function make_all_keys_unpressed(){
 }
 
 addEventListener("keydown", function(e){
-    if (e.key.toLowerCase() == 'a' || e.key.toLowerCase() == 'ф'){
+    let kp = e.key.toLowerCase();
+    if (kp == 'а' || kp == 'f'){
         make_all_keys_unpressed();
-        arguments('.do').style='background: red;';
+        a('.do').style='background: red;';
+        do_note.currentTime = 1;
+        do_note.play();
+    }
+    if (kp == 'б' || kp == ','){
+        make_all_keys_unpressed();
+        a('.re').style='background: red;';
+        re_note.currentTime = 1;
+        re_note.play();
+    }
+    if (kp == 'в' || kp == 'd'){
+        make_all_keys_unpressed();
+        a('.mi').style='background: red;';
+        mi_note.currentTime = 1;
+        mi_note.play();
+    }
+    if (kp == 'г' || kp == 'u'){
+        make_all_keys_unpressed();
+        a('.fa').style='background: red;';
+        fa_note.currentTime = 1;
+        fa_note.play();
+    }
+    if (kp == 'д' || kp == 'l'){
+        make_all_keys_unpressed();
+        a('.sol').style='background: red;';
+        sol_note.currentTime = 1;
+        sol_note.play();
+    }
+    if (kp == 'е' || kp == 't'){
+        make_all_keys_unpressed();
+        a('.lya').style='background: red;';
+        lya_note.currentTime = 1;
+        lya_note.play();
+    }
+    if (kp == 'ё' || kp == '`'){
+        make_all_keys_unpressed();
+        a('.si').style='background: red;';
+        si_note.currentTime = 1;
+        si_note.play();
+    }
+    if (kp == 'ж' || kp == ';'){
+        make_all_keys_unpressed();
+        a('.do1').style='background: red;';
+    }
+    if (kp == 'з' || kp == 'p'){
+        make_all_keys_unpressed();
+        a('.re1').style='background: red;';
     }
 });
---
+++ assets/js/sounds.js
@@ -0,0 +1,11 @@
+let audiof = ["noty-do", "re", "mi", "fa", "sol", "lya", "si"];
+
+var do_note = new Audio('/assets/sounds/noty-do.mp3');
+var re_note = new Audio('/assets/sounds/re.mp3');
+var mi_note = new Audio('/assets/sounds/mi.mp3');
+var fa_note = new Audio('/assets/sounds/fa.mp3');
+var sol_note = new Audio('/assets/sounds/sol.mp3');
+var lya_note = new Audio('/assets/sounds/lya.mp3');
+var si_note = new Audio('/assets/sounds/si.mp3');
+
+//do.play();
